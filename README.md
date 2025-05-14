34c1e50bcc746e518876835108d943498358ddf8
Benjamin Pailhes <bouilli_ji@protonmail.com>
2025-05-14 20:37:34 +0200

    ajout des informations pour le potentiometre dans le readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Robotcup2025-2026
+
+# potentiometre grove
+
+il faut le brancher sur les port A0,A2,A4,A6.
+on recupère les donner sur le pin avec le même numero que le port. (voir test_potentiometre.py)
--- test_potentiometre.py
+++ test_potentiometre.py
@@ -6,7 +6,7 @@ adc = ADC()
 try:
 	while True:
 		time.sleep(0.1)
-		value = adc.read(0)
+		value = adc.read(0) #le potentiometre sur A0
 		print("Valeur potenciomettre", value)
 except:
 	print("fin du programme")
