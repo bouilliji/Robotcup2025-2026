c72ba257c2d3ca3322e2e678f96f3d9051e152e3
Benjamin Pailhes <bouilli_ji@protonmail.com>
2025-05-14 20:30:29 +0200

    premier test avec un potentiometre grove

---
+++ test.py
@@ -0,0 +1,12 @@
+from grove.adc import ADC
+import time
+
+adc = ADC()
+
+try:
+	while True:
+		time.sleep(0.1)
+		value = adc.read(0)
+		print("Valeur potenciomettre", value)
+except:
+	print("fin du programme")
